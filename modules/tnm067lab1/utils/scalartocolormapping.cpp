b5ceb97079c13e9c1b1b7c36c62f7b4de949852c
Fredrik <fredrik96.johansson@gmail.com>
2020-09-15 18:14:08 +0200

    Lab1 task 1-8 done

--- modules/tnm067lab1/utils/scalartocolormapping.cpp
+++ modules/tnm067lab1/utils/scalartocolormapping.cpp
@@ -13,10 +13,27 @@ vec4 ScalarToColorMapping::sample(float t) const {
     // Interpolate colors in baseColors_
     // return the right values
 
-    if (t <= 0) return vec4(baseColors_.front());
-    if (t >= 1) return vec4(baseColors_.back());
+    float smallest_dist1 = __INT_MAX__;
+    float smallest_dist2 = __INT_MAX__;
+    int smallest_idx1 = 0;
+    int smallest_idx2 = 0;
 
     vec4 finalColor(t, t, t, 1);  // dummy color
+
+    for (size_t i = 0; i < baseColors_.size(); ++i) {
+        if(glm::distance(finalColor, baseColors_[i]) < smallest_dist1) {
+            smallest_dist1 = glm::distance(finalColor, baseColors_[i]);
+            smallest_idx1 = i;
+        } else if(glm::distance(finalColor, baseColors_[i]) < smallest_dist2) {
+            smallest_dist2 = glm::distance(finalColor, baseColors_[i]);
+            smallest_idx2 = i;
+        }
+    }
+
+    return ((1 - t) * baseColors_[smallest_idx1] + t * baseColors_[smallest_idx2]);
+
+    if (t <= 0) return vec4(baseColors_.front());
+    if (t >= 1) return vec4(baseColors_.back());
 
     return finalColor;
 }
