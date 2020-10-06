4b5768dd3b5db8d2b0fdadda5e2e1c60f7109894
Pontus Arnesson <arnesson.pontus@gmail.com>
2020-10-06 14:16:02 +0200

    Lab4 completed

--- modules/tnm067lab4/glsl/tensor_glyphrenderer.geom
+++ modules/tnm067lab4/glsl/tensor_glyphrenderer.geom
@@ -14,21 +14,26 @@ void main(void) {
     vec2 center = gl_in[0].gl_Position.xy;
 
     mat2 J = vJ[0];
+    mat2 Jsym = 0.5*(J + transpose(J));
 
     for (int i = 0; i < 32; i++) {
-        color = vec3(0.106, 0.620, 0.467);
+        if (i < 4 || i > 27) color = vec3(0.851, 0.373, 0.008);
+        else if (i < 12) color = vec3(0.106, 0.620, 0.467);
+        else if (i < 20) color = vec3(0.906,0.61,0.541);
+        else color = vec3(0.459,0.439,0.702);
+        
         float a1 = twoPi * (i / 32.0);        // angle from y-axis of second point in the triangle
         float a2 = twoPi * ((i + 1) / 32.0);  // angle from y-axis of third point in the triangle
 
         gl_Position = vec4(center, 0, 1);  // first point in triangle is in the origin of the circle
         EmitVertex();
 
-        vec2 o1 = vec2(0);  // calculate an offset vector for the second point in the triangle based on a1
+        vec2 o1 = Jsym * vec2(cos(a1), sin(a1));  // calculate an offset vector for the second point in the triangle based on a1
 
         gl_Position = vec4(center + o1 * radius, 0, 1);
         EmitVertex();
 
-        vec2 o2 = vec2(0);  // calculate an offset vector for the second point in the triangle based on a2
+        vec2 o2 = Jsym * vec2(cos(a2), sin(a2));  // calculate an offset vector for the second point in the triangle based on a2
 
         gl_Position = vec4(center + o2 * radius, 0, 1);
         EmitVertex();
--- modules/tnm067lab4/jacobian.cpp
+++ modules/tnm067lab4/jacobian.cpp
@@ -1,16 +1,45 @@
 #include <modules/tnm067lab4/jacobian.h>
 
-namespace inviwo {
+namespace inviwo
+{
 
-namespace util {
+    namespace util
+    {
 
-mat2 jacobian(const ImageSampler& sampler, vec2 position, vec2 offset) {
-    vec2 ox(offset.x, 0);
-    vec2 oy(0, offset.y);
-    mat2 J;  // TODO: Calculate the Jacobian J
-    return J;
-}
+        vec2 partialDerivateX(vec2 coord, vec2 offset, const ImageSampler &sampler)
+        {
+            float dx = offset.x;
+            vec2 plusx = sampler.sample(coord + vec2(dx, 0));
+            vec2 minusx = sampler.sample(coord + vec2(-dx, 0));
 
-}  // namespace util
+            vec2 partial_derivate = (plusx - minusx) / (2.0 * dx);
+            return partial_derivate;
+        }
 
-}  // namespace inviwo
+        vec2 partialDerivateY(vec2 coord, vec2 offset, const ImageSampler &sampler)
+        {
+            float dy = offset.y;
+            vec2 plusy = sampler.sample(coord + vec2(0, dy));
+            vec2 minusy = sampler.sample(coord + vec2(0, -dy));
+
+            vec2 partial_derivate = (plusy - minusy) / (2.0 * dy);
+            return partial_derivate;
+        }
+
+        mat2 jacobian(const ImageSampler &sampler, vec2 position, vec2 offset)
+        {
+            vec2 pdx = partialDerivateX(position, offset, sampler);
+            vec2 pdy = partialDerivateY(position, offset, sampler);
+
+            mat2 J; // = {pdx.x, pdx.y, pdy.x, pdy.y}; // TODO: Calculate the Jacobian J
+            J[0][0] = pdx.x;
+            J[0][1] = pdx.y;
+            J[1][0] = pdy.x;
+            J[1][1] = pdy.y;
+
+            return J;
+        }
+
+    } // namespace util
+
+} // namespace inviwo
